5ad623267062420879be0a337aa2c4cd7a58f5f9
Mostafa Hussein <mh225qg@student.lnu.se>
2023-02-16 15:43:51 +0100

    Test mobil
    Signed-off-by: Mostafa Hussein <mh225qg@student.lnu.se>

--- js/main.js
+++ js/main.js
@@ -8,3 +8,38 @@ navigator.serviceWorker.register("sw.js").then(registration =>{
 });
 
 }
+
+// JavaScript
+// Hämta nuvarande höjd med en API
+fetch('https://api.mymobileheight.com/v1/get-height')
+  .then(response => response.json())
+  .then(data => {
+    // Visa höjden i HTML
+    if (data.height) {
+      document.getElementById('height').innerHTML = data.height;
+    } else {
+      alert('Respons not viable');
+    }
+  });
+
+/*
+// Skapa en funktion som kontrollerar mobilens gravity censor
+function checkGravitySensor() {
+    // Kolla om enhetens enhet stödjer accelerometer
+    if (window.DeviceMotionEvent) {
+        // Registrera händelser för enheten
+        window.addEventListener("devicemotion", handleMotionEvent, true);
+    } else {
+        // Om enheten inte stöder accelerometer, skriv ut ett meddelande
+        document.getElementById("result").innerHTML = "Mobilen stödjer inte gravity censor";
+    }
+}
+
+// Skapa en funktion som hanterar händelsen
+function handleMotionEvent(event) {
+    // Skriv ut accelerationen som läses av sensorn
+    document.getElementById("result").innerHTML = "Acceleration i x-led: " + event.accelerationIncludingGravity.x + 
+    "<br>Acceleration i y-led: " + event.accelerationIncludingGravity.y +
+    "<br>Acceleration i z-led: " + event.accelerationIncludingGravity.z;
+}
+*/
