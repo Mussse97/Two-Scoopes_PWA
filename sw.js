69cbf5fe6b0b325ad3b178d3c483c074af29c328
Mussse97 <89797827+Mussse97@users.noreply.github.com>
2024-11-11 15:35:35 +0100

    Ändrad sökväg för sw för github

--- sw.js
+++ sw.js
@@ -1,51 +1,53 @@
 const cacheName = 'news-v1';
-// Filer som kan sparas i network
+// Filer som kan sparas i cache
 const staticAssets = [
-  './',
-  './index.html',
-  './Css/test.css',
-  './js/Motion.js',
-  './Css/choise.css',
-  './js/animation.js',
-  "./images/blur_strong.jpg",
-  "./choise.html",
+   './index.html',
+   './Css/test.css',
+   './js/Motion.js',
+   './Css/choise.css',
+   './js/animation.js',
+   './images/blur_strong.jpg',
+   './choise.html',
 ];
 
 self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
+   const cache = await caches.open(cacheName);
+   await cache.addAll(staticAssets);
+   return self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  self.clients.claim();
+   self.clients.claim();
 });
 
 self.addEventListener('fetch', async e => {
-  const req = e.request;
-  const url = new URL(req.url);
+   const req = e.request;
+   const url = new URL(req.url);
 
-  if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
-  } else {
-    e.respondWith(networkAndCache(req));
-  }
+   if (url.origin === location.origin) {
+      e.respondWith(cacheFirst(req));
+   } else {
+      e.respondWith(networkAndCache(req));
+   }
 });
 
+// cacheFirst-funktion med felhantering
 async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cached = await cache.match(req);
-  return cached || fetch(req);
+   const cache = await caches.open(cacheName);
+   const cached = await cache.match(req);
+   return cached || fetch(req).catch(() => console.error("Nätverksförfrågan misslyckades:", req.url));
 }
 
+// networkAndCache-funktion med felhantering
 async function networkAndCache(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cached = await cache.match(req);
-    return cached;
-  }
+   const cache = await caches.open(cacheName);
+   try {
+      const fresh = await fetch(req);
+      await cache.put(req, fresh.clone());
+      return fresh;
+   } catch (e) {
+      console.error("Nätverksförfrågan misslyckades och hämtas från cache:", req.url);
+      const cached = await cache.match(req);
+      return cached;
+   }
 }
